4c234d1b3801e00064f815f37d41b134aff17087
adelorosia <adel_orosia@yahoo.com>
2023-12-25 01:12:37 +0100

    logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,26 +2,6 @@ import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
-// GET ALL USER
-export const getAllUsers = asyncHandler(async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-export const getLoggedInUser = asyncHandler(async (req, res) => {
-  const id = req.userId;
-  const user =await User.findById(id);
-  if (user) {
-    res.json(user);
-  } else {
-    throw new Error("Gastbenutzer");
-  }
-});
-
 //REGISTER
 export const registerUser = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, userName, password } = req.body;
@@ -102,5 +82,42 @@ export const loginUser = asyncHandler(async (req, res) => {
   } else {
     res.status(401);
     throw new Error(`${loginType} or Password ist nicht Korrekt`);
+  }
+});
+
+//LOGOUT
+export const logoutUser = asyncHandler(async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) return res.json("keine Token gefunden");
+    const user = await User.findOne({ refresh_token: refreshToken });
+    if (!user) return res.json("keine user gefunden");
+    user.refresh_token = undefined;
+    await user.save();
+    res.clearCookie("refreshToken");
+    res.json("logout successfull");
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// GET ALL USER
+export const getAllUsers = asyncHandler(async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// Get LoggedIn User
+export const getLoggedInUser = asyncHandler(async (req, res) => {
+  const id = req.userId;
+  const user = await User.findById(id);
+  if (user) {
+    res.json(user);
+  } else {
+    throw new Error("Gastbenutzer");
   }
 });
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,7 @@ import {
   getAllUsers,
   getLoggedInUser,
   loginUser,
+  logoutUser,
   registerUser,
 } from "../controllers/userController.js";
 import { verifyToken } from "../middlewares/token/verifyToken.js";
@@ -10,10 +11,12 @@ import { refreshToken } from "../controllers/refreshToken.js";
 
 const router = express.Router();
 
-router.get("/api/token",refreshToken);
+router.get("/api/token", refreshToken);
 
 router.post("/api/register", registerUser);
 router.post("/api/login", loginUser);
+router.delete("/api/logout", logoutUser);
+
 router.get("/api/user/display", getAllUsers);
 router.get("/api/user/displayById", verifyToken, getLoggedInUser);
 
