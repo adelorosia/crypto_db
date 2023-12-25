dfbb2e7d14105f29c2c14d43cbabc2469dfa9e79
adelorosia <adel_orosia@yahoo.com>
2023-12-25 11:18:05 +0100

    cookie einstellung

--- controllers/userController.js
+++ controllers/userController.js
@@ -77,6 +77,8 @@ export const loginUser = asyncHandler(async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
+      secure:true,
+      sameSite:'none'
     });
     res.json({ accessToken });
   } else {
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app=express()
 const port=process.env.PORT || 5001
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({Credential:true,origin:"http://localhost:3000"}))
 app.use(cookieParser())
 
 
